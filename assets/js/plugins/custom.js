e06be6e3eb0da2bc0a6d59d27888a4c4221d71d9
Your Name <deepak@singhsoft.com>
2023-12-29 10:01:51 +0530

    update image and url

---
+++ assets/js/plugins/custom.js
@@ -0,0 +1,15 @@
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const sliderTrack = document.querySelector('.slider-track');
+//     let currentIndex = 0;
+
+//     // Example: Change slide every 3 seconds
+//     setInterval(function () {
+//         currentIndex += 1;
+//         if (currentIndex >= sliderTrack.children.length / 2) {
+//             currentIndex = 0;
+//         }
+//         sliderTrack.style.transform = `translateX(-${100 * currentIndex}%)`;
+//     }, 3000); // 3 seconds
+// });
